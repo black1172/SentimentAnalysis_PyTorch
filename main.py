8b9f51d0b90dc750f139a65d66ed40710e26052b
black1172 <black.1172@buckeyemail.osu.edu>
2025-07-17 16:21:38 -0400

    first try at training loop

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 from model import SentimentRNN
 from tokenizer import SimpleTokenizer
+import data as d
 
-# Create tokenizer first
-tokenizer = SimpleTokenizer()
-tokenizer.build_vocab(your_reviews)  # Build vocabulary
+# Get data
+reviews = d.get_sample_data()
 
-# Now create model with specific sizes
-vocab_size = len(tokenizer.word_to_id)  # Number of unique words
-embed_size = 50    # Size of word vectors
-hidden_size = 64   # Size of RNN memory
-
-model = SentimentRNN(vocab_size, embed_size, hidden_size)
-from tokenizer import SimpleTokenizer
-
-# Sample movie reviews
-reviews = [
-    "This movie was great!",
-    "Bad film, not good",
-    "Amazing movie, really loved it",
-    "Terrible, worst movie ever"
-]
-
-# Test it
+# Build maps
 tokenizer = SimpleTokenizer()
 tokenizer.build_vocab(reviews)
 
+# Create model with specific sizes
+vocab_size = len(tokenizer.word_to_id)  # Number of unique words
+embed_size = 50    # Size of word vectors
+hidden_size = 64   # Size of RNN memory
+model = SentimentRNN(vocab_size, embed_size, hidden_size)
+
+# Display Vocab
 print("Vocabulary:", tokenizer.word_to_id)
-print("\nTokenizing 'This movie rocks!':")
-tokens = tokenizer.tokenize("This movie rocks!")
-print("Tokens:", tokens)
+
+# Training loop
+for review in reviews:
+    tokenized_review = tokenizer.tokenize(review)
+    prediction = model(tokenized_review)
+    
+
+    
